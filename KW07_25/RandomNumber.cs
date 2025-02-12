83af18ad4356beb302229073a990b55aacbe5b99
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-02-12 14:20:46 +0100

    add RandomNumber Code

--- KW07_25/RandomNumber.cs
+++ KW07_25/RandomNumber.cs
@@ -25,7 +25,8 @@ namespace ILA24_M320_2.Semester.KW07_25
                 string myString = Console.ReadLine();
                 isNumber = int.TryParse(myString, out number);
 
-                if (isNumber || number > 1 || number < 10)
+                if (isNumber = false || number < 1 || number > 10)
+
                 {
                     Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl zwischen" +
                         "1 und 10 ein:");
