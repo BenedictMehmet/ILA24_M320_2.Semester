11951693db1052a08c9cef5e173b72f87779ff1d
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-04-09 13:43:52 +0200

    add list2 class

--- KW15_25/List2.cs
+++ KW15_25/List2.cs
@@ -10,8 +10,10 @@ namespace ILA24_M320_2.Semester.KW15_25
     {
         static void Main(string[]args)
         {
-            List<string, int> products = new List<string, int>()
+            List<(string, int)> products = new List<(string, int)>
             {
+                ("Zitrone", 4)
+
 
             };
         }
