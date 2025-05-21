635689952757d54ae0bc53f8c16abfeee6fb9f9d
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-05-21 11:26:42 +0200

    add tasks to week21

---
+++ KW21_25/Aufgaben/Aufgaben.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILA24_M320_2.Semester.KW21_25.Aufgaben
+{
+    internal class Aufgaben
+    { /*
+     * Aufgabe 1: Gerade oder ungerade?
+     * --------------------------------
+     * Erstelle eine Methode 'IstGerade', die eine ganze Zahl als Parameter nimmt.
+     * Die Methode soll true zurückgeben, wenn die Zahl gerade ist, sonst false.
+     * Rufe die Methode in Main mit verschiedenen Zahlen auf.
+     * Gib aus: "Die Zahl X ist gerade" oder "Die Zahl X ist ungerade".
+     */
+
+     
+
+        /*
+         * Aufgabe 2: Einfacher Taschenrechner
+         * -----------------------------------
+         * Erstelle Methoden für Addieren, Subtrahieren, Multiplizieren und Dividieren.
+         * Lies zwei Zahlen und ein Rechenzeichen (+, -, *, /) vom Benutzer ein.
+         * Nutze switch, um die passende Methode aufzurufen.
+         * Gib das Ergebnis aus.
+         */
+
+       
+
+        /*
+         * Aufgabe 3: Passwort prüfen
+         * --------------------------
+         * Erstelle eine Methode 'PruefePasswort', die einen String entgegennimmt.
+         * Wenn der String "geheim123" entspricht, soll true zurückgegeben werden.
+         * Sonst false. In Main soll die Eingabe abgefragt und das Ergebnis ausgegeben werden.
+         */
+
+    
+
+        /*
+         * Aufgabe 4: Zahlenraten mit Schleife
+         * -----------------------------------
+         * Erstelle eine Methode 'ZahlRaten'.
+         * Generiere eine Zufallszahl zwischen 1 und 10.
+         * Lass den Benutzer raten, bis er die Zahl trifft.
+         * Wenn falsch: "Falsch, versuch es nochmal."
+         * Wenn richtig: "Richtig! Du hast die Zahl erraten."
+         * Verwende do-while-Schleife.
+         */
+    }
+}
